5d09d8025f8bcdb55498c99a3854f44075744621
Gong, Wen <wen.g.gong@gmail.com>
2025-03-15 01:02:32 -0400

    play

--- cookbook/models/ollama/basic.py
+++ cookbook/models/ollama/basic.py
@@ -1,11 +1,16 @@
 from agno.agent import Agent, RunResponse  # noqa
 from agno.models.ollama import Ollama
 
-agent = Agent(model=Ollama(id="llama3.1:8b"), markdown=True)
+model_id = "llama3.1"
+agent = Agent(
+            model=Ollama(id=model_id), 
+            markdown=True,
+        )
 
 # Get the response in a variable
 # run: RunResponse = agent.run("Share a 2 sentence horror story")
 # print(run.content)
 
 # Print the response in the terminal
-agent.print_response("Share a 2 sentence horror story")
+user_msg = "write me a poem about Chinese character"
+agent.print_response(user_msg, stream=True)
